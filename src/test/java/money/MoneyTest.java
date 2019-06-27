b4c442421b4f30e673eb24ed8f9515f36b47416d
NaoyaKurahashi <naoya.kurahashi@bizreach.co.jp>
2019-06-27 19:20:56 +0900

    JUnit でテスト動作させるための雛形を作成した

---
+++ src/test/java/money/MoneyTest.java
@@ -0,0 +1,16 @@
+package money;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author maro
+ */
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        assertEquals(10, 10);
+    }
+}
